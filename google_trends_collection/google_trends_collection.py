018f1aa9e87b529989bf7d45ddd955cf347bc60a
tsennikova <tanya.sennikova@gmail.com>
2016-07-07 12:48:42 +0200

    google trends code and data

--- google_trends_collection/google_trends_collection.py
+++ google_trends_collection/google_trends_collection.py
@@ -17,34 +17,45 @@ neighbors_dir = os.path.join(data_dir, 'neighbors')
 google_trends_dir = os.path.join(data_dir, 'google_trends')
 scientisis_dir = os.path.join(google_trends_dir, 'scientists\\')
 topics_dir = os.path.join(google_trends_dir, 'topics\\')
+errorfile =  os.path.join(topics_dir, 'error_list.txt')
 
 def google_trend_crawler(name):
+    #suggestions = {}
     google_username = "nobelprize.mt"
     google_password = "googletrendsparser"
     title = name.replace('_', ' ')
-    title = title.rstrip().split('(')[0]
-    title = title.decode("cp1252")
-    #title = urllib.quote_plus(title.decode("utf-8"))
-    title = title.encode('cp1252')
+    title = title.translate(None, '():')
+    title = urllib.unquote(title)
+    title = title.decode('utf-8')
+    title = title.encode("cp1252")
+
+    
     connector  = pyGTrends(google_username, google_password)
     connector.request_report(title, hl='en-US')
-    # wait a random amount of time between requests to avoid bot detection
-    time.sleep(randint(5, 10))
-    name = name.translate(None, ':*\'')
-    name = name.decode("cp1252")
-    name = urllib.quote_plus(name.encode("utf-8"))
-    connector.save_csv("", name)
+    suggestions = connector.get_suggestions(title)
+    if suggestions['default']['topics'] != []:
+        title = suggestions['default']['topics'][0].get('mid')
+        # wait a random amount of time between requests to avoid bot detection
+        time.sleep(randint(5, 10))
+        print title
+        connector.request_report(title, hl='en-US')
+        name = name.translate(None, ':*\'')
+        name = name.decode("cp1252")
+        name = urllib.quote_plus(name.encode("utf-8"))
+        connector.save_csv(topics_dir, name)
+    else:
+        with open(errorfile, "a") as myfile:
+            myfile.write("%s\n" % name)
     return
 
+
 counter = 0
-filename =  os.path.join(neighbors_dir, 'topics_list_(spec_encoding)-1.txt')
+filename =  os.path.join(neighbors_dir, 'topics_list-1.txt')
+
+
 with open(filename) as f:
     link_list = f.read().splitlines()   
 
 for link in link_list:
     counter +=1
-    print link
-    
-    print link.decode("cp1252").encode("utf-8")
-    print counter
     google_trend_crawler(link)
