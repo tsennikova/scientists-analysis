e7674da8688ee1a2ce1976b80a617ed8f49b2213
tsennikova <tanya.sennikova@gmail.com>
2016-07-19 14:11:37 +0200

    code and plots for attention distribution

---
+++ plotting/attention_distribution.py
@@ -0,0 +1,123 @@
+'''
+Created on 18 Jul 2016
+
+@author: sennikta
+'''
+
+
+import json
+import os
+from datetime import datetime
+import numpy
+import matplotlib.pyplot as plt
+import csv
+
+from itertools import islice
+
+base_dir = os.path.dirname(os.path.dirname(__file__))
+data_dir = os.path.join(base_dir, 'data')
+seed_dir = os.path.join(data_dir, 'seed')
+baseline_dir = os.path.join(data_dir, 'baseline')
+plots_dir = os.path.join(data_dir, 'plots')
+powerlaw_dir = os.path.join(plots_dir, 'attention_distribution')
+
+# Change address for each dataset
+edits_dir = os.path.join(data_dir, 'edits')
+scientists_dir = os.path.join(edits_dir, 'scientists')
+
+def load_simple_json(filename):
+    print filename
+    with open(filename, 'r') as f:
+        return json.load(f)
+    
+
+# Plot the frequency distribution
+def plot_distribution(seed, baseline, name):
+    plt.title("Attention distribution (views)")
+    plt.xlabel("# of searches (views)")
+    plt.ylabel("probability")
+    bins = range(10, 25)
+    plt.xticks(bins, ["2^%s" % i for i in bins])
+    plt.hist(numpy.log2(seed), log=True, normed = True, bins=bins, label = 'seed data', alpha=0.5,)
+    plt.hist(numpy.log2(baseline), log=True, normed = True, bins=bins, label = 'baseline data', alpha=0.5,)
+    plt.legend(loc='upper left')
+    plt.savefig(powerlaw_dir+name)
+    return
+
+# Box Plot 
+def box_plot(seed, baseline, name):
+    data_to_plot = [seed, baseline]
+    fig = plt.figure(1, figsize=(9, 4))
+    ax = fig.add_subplot(111)
+    bp = ax.boxplot(data_to_plot)
+    
+    plt.title("Attention distribution (edits)")
+    ax.set_xticklabels(['seed data', 'baseline'])
+    plt.ylabel("# of edits")
+    bins = range(10, 25)
+    #plt.show()
+    plt.savefig(powerlaw_dir+name)
+    return
+    
+
+
+# For views and edits
+def read_txt(filename):
+    scientist_dict = load_simple_json(filename)
+    data_list = []
+    attention_value = 0
+    attention_list = []
+    for scientist in scientist_dict:
+
+        scientist = scientist.rstrip().split('/')[-1]
+        txtname = os.path.join(scientists_dir + '\\' + scientist + '.txt')   
+        try:
+            f = open(txtname)
+            for line in f:
+                time_list = map(int, line.split(','))
+                time_list.pop(0)
+                attention_value += sum(time_list)
+            f.close()
+            attention_list.append(attention_value)
+        except IOError:
+            print scientist
+            continue
+    return attention_list
+
+
+# For google trends
+def read_csv(filename):
+    scientist_dict = load_simple_json(filename)
+    data_list = []
+    attention_value = 0
+    attention_list = []
+    for scientist in scientist_dict:
+        scientist = scientist.rstrip().split('/')[-1]
+        csvname = os.path.join(scientists_dir + '\\' + scientist + '.csv')
+        try: 
+            f = open(csvname, 'rb')
+            reader = csv.reader(f)
+            # skip template
+            for row in islice(reader, 5, 657):
+                attention_value += int(row[1])
+            f.close()
+            attention_list.append(attention_value)
+        except IOError:
+            print scientist
+            continue        
+    return attention_list
+
+# for views and edits
+filename =  os.path.join(seed_dir, 'seed_creation_date.json')    
+seed = read_txt(filename)
+filename =  os.path.join(baseline_dir, 'baseline_creation_date.json')  
+baseline = read_txt(filename)
+
+# for google trends
+#filename =  os.path.join(seed_dir, 'seed_creation_date.json')    
+#seed = read_csv(filename)
+#filename =  os.path.join(baseline_dir, 'baseline_creation_date.json')  
+#baseline = read_csv(filename)
+
+
+box_plot(seed, baseline, '/boxplot_edits.pdf')
