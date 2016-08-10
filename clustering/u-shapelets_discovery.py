a919d3a1bd5a791eb588baef13ab1309c89e8dda
tsennikova <tanya.sennikova@gmail.com>
2016-08-10 15:37:34 +0200

    u-shapelets disovery code for scientists (working)

--- clustering/u-shapelets_discovery.py
+++ clustering/u-shapelets_discovery.py
@@ -11,7 +11,7 @@ from os import listdir
 import numpy
 import json
 import random
-
+from collections import defaultdict
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 #---------------- Define directories ---------------------- Define directories ---------------- Define directories --------------------------------------
@@ -64,9 +64,9 @@ def random_masking(sax_list):
     print 'in random masking function'
     R = 10
     maskedNum = 3
-    candidate_dict = {}
-    masked_list = []
+    candidate_dict = defaultdict(list)
     for i in range (0,R):
+        masked_list = []
         stop_list = []
         mask_ind = random.sample(range(0, 8), 3)
         masked_dict = {}
@@ -83,8 +83,6 @@ def random_masking(sax_list):
                 masked_time_series.append(masked_word)
             masked_time_series = set(masked_time_series)
             masked_list.append(masked_time_series)
-        print 'random mask #', i, '=', mask_ind
-        print 'masked time series', masked_time_series
         
         print 'counting frequency'
         for time_series in sax_list:
@@ -98,20 +96,15 @@ def random_masking(sax_list):
                         del masked_candidate[l]
                     masked_candidate = int(''.join(masked_candidate))
                 
-                   # Critical point
+                   # Critical point                    
                     for masked_time_series in masked_list:
-                        #masked_time_series=set(masked_time_series)
                         if masked_candidate in masked_time_series:
                             sum+=1
-                            print sum
-                    
-                    candidate_dict.update({str(candidate):sum_list.append(sum)})
+                    candidate_dict[str(candidate)].append(sum)
                     
                     stop_list.append(candidate)
-        print 'candidate dir:'
-        print candidate_dict
         
-        #print i
+        print i
     #print candidate_dict
     return candidate_dict
 
@@ -121,22 +114,22 @@ sax_list = []
 scientist_dict = load_simple_json(scientists_file)
 
 # FOR TESTING
-scientist_list=['1.txt','2.txt','3.txt','4.txt','5.txt']
-for scientist in scientist_list:
-    filename = os.path.join(test + "\\"+scientist)
-    ts_list=read_sax(filename)
-    sax_list.append(ts_list)
-print 'SAX list:', sax_list
-candidate_dict = random_masking(sax_list)
 
-# REAL CODE
-
-# for scientist in scientist_dict:
-#     scientist = scientist.rstrip().split('/')[-1]
-#     filename = os.path.join(dir + '\\' + scientist + '.txt')
+# scientist_list=['1.txt','2.txt','3.txt','4.txt','5.txt']
+# for scientist in scientist_list:
+#     filename = os.path.join(test + "\\"+scientist)
 #     ts_list=read_sax(filename)
 #     sax_list.append(ts_list)
 # candidate_dict = random_masking(sax_list)
-# 
-# with open('candidate_list.json', 'w') as f:
-#         json.dump(candidate_dict, f, indent=4, sort_keys=True)
+
+# REAL CODE
+
+for scientist in scientist_dict:
+    scientist = scientist.rstrip().split('/')[-1]
+    filename = os.path.join(dir + '\\' + scientist + '.txt')
+    ts_list=read_sax(filename)
+    sax_list.append(ts_list)
+candidate_dict = random_masking(sax_list)
+ 
+with open('candidate_list.json', 'w') as f:
+        json.dump(candidate_dict, f, indent=4, sort_keys=True)
